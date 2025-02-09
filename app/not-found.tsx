dfd83de7594790fe123a01bd4a5de62effecfa08
Einja <ethanfan1033@gmail.com>
2025-02-08 19:09:33 -0500

    '

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound: React.FC = () => {
       <h1 className="text-6xl mt-60">404</h1>
       <h1 className="text-3xl mt-10 p-6">THERE IS NOTHING HERE!!!</h1>
       <p className="w-1/2 text-center mb-10">
-        Either you entered an invalid URL, or the dev hasn't finished
+        Either you entered an invalid URL, or the dev has not finished
         implementing this. Please tell him at to stop being lazy at
         emf5690@psu.edu
       </p>
